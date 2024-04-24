34cbbeb0ec75f3ace5414372658013ff4a178308
Kothai_S <102270819+JebaMercy14@users.noreply.github.com>
2024-04-24 11:29:41 +0530

    Delete Management directory

--- Management/src/main/java/com/pharmacy/Management/repository/SupplierRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pharmacy.Management.repository;
-
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-import com.pharmacy.Management.models.*;
-
-public interface SupplierRepository extends JpaRepositoryImplementation<Supplier, Integer> {
-
-}
